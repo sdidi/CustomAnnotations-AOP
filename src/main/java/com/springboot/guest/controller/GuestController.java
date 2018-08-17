bc4e451b037d8cfc39af86bab28e0ca81ee86a40
Sabre <Sabredd0@gmail.com>
2018-08-17 18:09:11 +0200

    first commit

--- src/main/java/com/springboot/guest/controller/GuestController.java
+++ src/main/java/com/springboot/guest/controller/GuestController.java
@@ -7,8 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,10 +40,17 @@ public class GuestController {
 	}
 	
 	
-	/*
-	@RequestMapping(value = "/", method=RequestMethod.GET)
-	public String getStartPage(Model model) {
-		return "home";
-	}*/
+	@PutMapping("save")
+	public ResponseEntity<Guest> updateArticle(@RequestBody Guest guest) {
+		guestService.saveOrUpdate(guest);
+		return new ResponseEntity<Guest>(guest, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("guestD/{id}")
+	public ResponseEntity<Void> deleteArticle(@PathVariable("id") Long id) {
+		guestService.delete(id);
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+	}	
+	
 
 }
